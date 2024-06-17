61cce1d665d0085b7b373df5211bb327605a6e62
Guilherme Machado <136522300+guilhermesvm@users.noreply.github.com>
2024-06-17 10:54:33 -0300

    Delete PostgreSQL directory

--- PostgreSQL/university (admin)/Escola (zezinho).sql
+++
@@ -1,6 +0,0 @@
--- AMBIENTE DO ZEZINHO
-
-SELECT * FROM Aluno -- Sem permissao
-
-SELECT * FROM professor -- Com permissao
-UPDATE professor SET nome = 'Wilma da UPF' WHERE nome = 'Wilma da UPF'; -- error
